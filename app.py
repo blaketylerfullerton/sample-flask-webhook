c2ef59632ab3441839116baa69bd61419a721d28
Blake Fullerton <blakefullerton@Blakes-Laptop.local>
2024-08-05 20:43:14 -0700

    new endpoint

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+    
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print("Received data:")
+        print(request.json)
+        return jsonify({"status": "success"}), 200
+    else:
+        return jsonify({"status": "error", "message": "Method not allowed"}), 405
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
